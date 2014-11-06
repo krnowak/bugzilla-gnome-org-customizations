8a98142043b5cf60d36f03ae367e9bd0d85c4e4e
Krzesimir Nowak <qdlacz@gmail.com>
2014-11-06 23:29:23 +0100

    patch report: Update config to be similar to generated one

--- PatchReport/Config.pm
+++ PatchReport/Config.pm
@@ -1,5 +1,12 @@
 package Bugzilla::Extension::PatchReport;
 use strict;
+
 use constant NAME => 'PatchReport';
+
+use constant REQUIRED_MODULES => [
+];
+
+use constant OPTIONAL_MODULES => [
+];
 
 __PACKAGE__->NAME;
