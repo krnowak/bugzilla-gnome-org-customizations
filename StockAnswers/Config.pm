634b115ecf2202999038f38fd2d80d4c1c929807
Krzesimir Nowak <qdlacz@gmail.com>
2014-11-08 21:31:14 +0100

    stock answers: Fix miscellaneous issues
    Whitespace cleanups, use warnings pragma, set extension version.

--- StockAnswers/Config.pm
+++ StockAnswers/Config.pm
@@ -1,5 +1,6 @@
 package Bugzilla::Extension::StockAnswers;
 use strict;
+use warnings;
 use constant NAME => 'StockAnswers';
 
 __PACKAGE__->NAME;
--- StockAnswers/Extension.pm
+++ StockAnswers/Extension.pm
@@ -1,11 +1,8 @@
 package Bugzilla::Extension::StockAnswers;
 use strict;
+use warnings;
 use base qw(Bugzilla::Extension);
 
-
-
-our $VERSION = '';
-
-
+our $VERSION = '0.01';
 
 __PACKAGE__->NAME;
