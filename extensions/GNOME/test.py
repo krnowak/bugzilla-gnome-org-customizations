31d395e989192c28181bb2b5dff42b9044faa06e
Olav Vitters <olav@vitters.nl>
2015-01-19 11:59:03 +0100

    test against test server

--- extensions/GNOME/test.py
+++ extensions/GNOME/test.py
@@ -2,9 +2,9 @@
 
 import xmlrpclib
 import sys
-server = xmlrpclib.ServerProxy('http://127.0.0.1/xmlrpc.cgi')
+server = xmlrpclib.ServerProxy('https://bugzilla-test.gnome.org/xmlrpc.cgi')
 try:
-    result = server.GNOME.addversionx({'product': 'lalala', 'version': '1.2.3.4'})
+    result = server.GNOME.addversionx({'product': 'foobar', 'version': '1.2.3.4'})
 except xmlrpclib.Fault, e:
     print "FAILED (%s)" % e.faultString
     sys.exit(1)
